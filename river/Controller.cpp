a8d4bac8f5f63b4200ce3ad17d46965263ffeffe
MazieroMarco <maziero.marco@gmail.com>
2021-04-14 16:22:02 +0200

    Added initial files

---
+++ river/Controller.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Marco on 14.04.2021.
+//
+
---
+++ river/River.h
@@ -0,0 +1,8 @@
+//
+// Created by Marco on 14.04.2021.
+//
+
+#ifndef POO2_L3_RIVIERE_RIVER_H
+#define POO2_L3_RIVIERE_RIVER_H
+
+#endif //POO2_L3_RIVIERE_RIVER_H
